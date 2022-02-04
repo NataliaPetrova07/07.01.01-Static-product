ce3742b31ee5f223938f94e85bf3d874d8d254c1
Natalia Petrova <petrova.ntl@gmail.com>
2022-02-04 13:40:53 +0100

    2st dynamic product iteration

---
+++ product.js
@@ -0,0 +1,13 @@
+const url = "https://kea-alt-del.dk/t7/api/products/1525";
+
+fetch(url)
+.then(res => res.json())
+.then(data => showProduct(data));
+ 
+function showProduct(product){
+    console.log(product);
+    document.querySelector(".breadcrumbs .category").textContent=product.category;
+    document.querySelector(".breadcrumbs .productname").textContent=product.productdisplayname;
+    document.querySelector("img.productimage").src=`https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+    document.querySelector("img.productimage").alt=product.productdisplayname;
+}
